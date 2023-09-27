328c6ea7d81b254ce7128c3c972b8e2bcb3f446d
ngongocthang <142952742+ngongocthang@users.noreply.github.com>
2023-09-27 08:13:05 +0700

    Add files via upload

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+var a=parseFloat(prompt("nhap a :"));
+var b=parseFloat(prompt("nhap b :"));
+var tong=a+b
+var hieu=a-b
+var tich=a*b
+var thuong=a/b
+document.writeln("tong : "+tong+"<br>");
+document.writeln("hieu : "+hieu+"<br>");
+document.writeln("tich : "+tich+"<br>");
+document.writeln("thuong : "+thuong+"<br>");
+
+
+var c1= a>b;
+document.writeln("SO SANH 1 :"+c1+"<BR>");
+var c2= a<b;
+document.writeln("SO SANH 2 :"+c2+"<BR>");
+var c3= a>=b;
+document.writeln("SO SANH 3 :"+c3+"<BR>");
+var c4= a<=b;
+document.writeln("SO SANH 4 :"+c4+"<BR>");
+
+
